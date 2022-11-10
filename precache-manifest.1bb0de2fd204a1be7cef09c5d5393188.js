1cd212d7fb15c842d5916f12ea0717a1300e1218
nitin <nitintiwari314@gmail.com>
2022-11-10 20:58:17 +0530

    Updates

---
+++ precache-manifest.1bb0de2fd204a1be7cef09c5d5393188.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "08a5b7fbbece3f4d909f",
+    "url": "/reactQuiz/static/css/main.446bf882.chunk.css"
+  },
+  {
+    "revision": "08a5b7fbbece3f4d909f",
+    "url": "/reactQuiz/static/js/main.08a5b7fb.chunk.js"
+  },
+  {
+    "revision": "dcb7df9bbfde9dd1c6b0",
+    "url": "/reactQuiz/static/js/1.dcb7df9b.chunk.js"
+  },
+  {
+    "revision": "aab3ee76d685eb845dd2",
+    "url": "/reactQuiz/static/js/2.aab3ee76.chunk.js"
+  },
+  {
+    "revision": "caf0558838a5ca655930",
+    "url": "/reactQuiz/static/js/runtime~main.caf05588.js"
+  },
+  {
+    "revision": "f3df365115e256207224bcea1f745616",
+    "url": "/reactQuiz/index.html"
+  }
+];
